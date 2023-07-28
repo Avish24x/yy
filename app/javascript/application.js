1995ff604b4a84e9eb4ee5c278efbcb93d4edc08
milesh taukoorah <mileshtaukoorah@gmail.com>
2023-07-28 04:12:43 -0700

    added view password on sign up page

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,3 +2,24 @@
 import "@hotwired/turbo-rails"
 import "./controllers"
 import * as bootstrap from "bootstrap"
+
+
+// app/assets/javascripts/application.js
+document.addEventListener("DOMContentLoaded", function () {
+  const passwordField = document.getElementById("user_password");
+  const passwordToggle = document.getElementById("password-toggle");
+
+  if (passwordToggle) {
+    passwordToggle.addEventListener("click", function () {
+      if (passwordField.type === "password") {
+        passwordField.type = "text";
+        passwordToggle.classList.remove("fa-eye");
+        passwordToggle.classList.add("fa-eye-slash");
+      } else {
+        passwordField.type = "password";
+        passwordToggle.classList.remove("fa-eye-slash");
+        passwordToggle.classList.add("fa-eye");
+      }
+    });
+  }
+});
